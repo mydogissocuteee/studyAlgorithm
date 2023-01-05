9983c2d4e3c651d48479c22a7f9b26e3eb7c42e1
seoyeon <dltjdus0725@naver.com>
2023-01-05 22:57:52 +0900

    0105

---
+++ trainingBaekjoon/src/trainingBaekjoon/d0105_dfs.java
@@ -0,0 +1,23 @@
+package trainingBaekjoon;
+
+public class d0105_dfs {
+	int[] numbers;
+	int target;
+	int answer;
+	
+	void dfs (int index, int sum) {
+		
+		if( index == answer) {
+			if(sum == target) answer++;
+			return;
+		}
+		dfs(index +1 , sum + numbers[index]);
+		dfs(index +1 , sum - numbers[index]);
+	}
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
