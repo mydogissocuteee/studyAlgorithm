93f55f6caf306065a7ffd1ed6774fbe2fe3e68c2
seoyeon <dltjdus0725@naver.com>
2023-01-10 15:32:39 +0900

    0110
    Change-Id: Ia5f9ffddad6441fe4b9e8757e7e3faac71f2852a

--- trainingBaekjoon/src/trainingBaekjoon/nd0106_1260.java
+++ trainingBaekjoon/src/trainingBaekjoon/nd0106_1260.java
@@ -1,5 +1,7 @@
 package trainingBaekjoon;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class nd0106_1260 {
@@ -7,13 +9,39 @@ public class nd0106_1260 {
 	static int nn; //노드 수
 	static int mm; //간선 수
 	static int vv; //시작 정점 
-	static int[][] graph;
+	static int[][] graph; //간선 연결 상태
 	static boolean[] checked; //확인 여부
 	
 	public static void dfs(int i) {
 		checked[i] = true;
-		
+		System.out.print(i + " ");
+	    
+	    for(int j = 1; j <= nn; j++) {
+	      if(graph[i][j] == 1 && checked[j] == false) {
+	        dfs(j);
+	      }
+	    }
 	}
+	
+	public static void bfs() {
+	    Queue<Integer> queue = new LinkedList<Integer>();
+	    queue.offer(vv); //시작점도 Queue에 넣어야 함
+	    checked[vv] = true;
+	    System.out.print(vv + " ");
+	    
+	    //Queue가 빌 때까지 반복. 방문 정점은 확인, 출력 후 Queue에 넣어 순서대로 확인
+	    while(!queue.isEmpty()) {
+	      int temp = queue.poll();
+	      
+	      for(int j = 1; j <= nn; j++) {
+	        if(graph[temp][j] == 1 && checked[j] == false) {
+	          queue.offer(j);
+	          checked[j] = true;
+	          System.out.print(j + " ");
+	        }
+	      }
+	    }
+	  }
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
@@ -21,7 +49,7 @@ public class nd0106_1260 {
 		mm = in.nextInt();
 		vv = in.nextInt();
 		graph = new int[nn+1][nn+1];
-		
+		checked = new boolean[nn+1];
 		
 		for (int i =0;i<mm;i++) {
 			int fromn = in.nextInt();
@@ -38,6 +66,13 @@ public class nd0106_1260 {
 			System.out.println();
 				
 		}
+		
+		  dfs(vv); //dfs호출
+		  
+		  checked = new boolean[1001]; //확인상태 초기화
+		  System.out.println(); //줄바꿈
+		  
+		  bfs(); //bfs호출
 
 	}
 
