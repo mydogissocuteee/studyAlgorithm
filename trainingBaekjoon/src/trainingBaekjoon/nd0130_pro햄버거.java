d5212145662d5076c6f7509b54fb0570af141d65
seoyeon <dltjdus0725@naver.com>
2023-01-30 23:49:30 +0900

    0130
    Change-Id: Ib16702207b0188468fccbb2bd501337e724212cb

---
+++ trainingBaekjoon/src/trainingBaekjoon/nd0130_pro햄버거.java
@@ -0,0 +1,12 @@
+package trainingBaekjoon;
+
+public class nd0130_pro햄버거 {
+	public static void main(String[] args) {
+		int[] ingredient = {2, 1, 1, 2, 3, 1, 2, 3, 1};
+		System.out.println(solution(ingredient));
+	}
+	public static int solution(int[] ingredient) {
+        int answer = 0;
+        return answer;
+    }
+}
