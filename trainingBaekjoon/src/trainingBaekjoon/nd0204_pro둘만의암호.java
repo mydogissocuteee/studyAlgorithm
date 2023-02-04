fbe556795cfe901f626be08b39232fe5b6bb3d55
seoyeon <dltjdus0725@naver.com>
2023-02-05 00:01:22 +0900

    02
    Change-Id: I86e9262e0d730a3e9238f91b8d29f2af75986cfc

---
+++ trainingBaekjoon/src/trainingBaekjoon/nd0204_pro둘만의암호.java
@@ -0,0 +1,21 @@
+package trainingBaekjoon;
+
+import java.util.ArrayList;
+
+public class nd0204_pro둘만의암호 {
+	public static void main(String[] args) {
+		System.out.println("결과"+solution("aukks",	"wbqd",	5));
+		System.out.println();
+	}
+	
+	public static String solution(String s, String skip, int index) {
+        String answer = "";
+        char[] arr = s.toCharArray();
+        for(int i=0;i<s.length();i++) {
+        	
+        }
+        
+        return answer;
+    }
+	
+}
